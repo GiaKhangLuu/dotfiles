7645ca161f0dfa3f13ae4f13313d1e05a7bc76e2
GiaKhangLuu <waoknight@gmail.com>
2022-10-20 16:26:34 +0700

    update

--- install.sh
+++ install.sh
@@ -165,15 +165,15 @@ function link_dotfiles {
   ln -s $(pwd)/.vimrc ~/.vimrc
   #ln -s $(pwd)/vimrc.bundles ~/.vimrc.bundles
 
-  rm -rf $HOME/.config/nvim/init.vim
-  rm -rf $HOME/.config/nvim
-  rm -rf $HOME/.vim/bundle/*
+  #rm -rf $HOME/.config/nvim/init.vim
+  #rm -rf $HOME/.config/nvim
+  #rm -rf $HOME/.vim/bundle/*
 
-  mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
+  #mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
 
-  ln -s $(pwd)/vim $XDG_CONFIG_HOME/nvim
-  ln -s $(pwd)/vimrc $XDG_CONFIG_HOME/nvim/init.vim
-  ln -s $(pwd)/schemes/dracula.zsh-theme $HOME/.oh-my-zsh/themes/dracula.zsh-theme
+  #ln -s $(pwd)/vim $XDG_CONFIG_HOME/nvim
+  #ln -s $(pwd)/vimrc $XDG_CONFIG_HOME/nvim/init.vim
+  #ln -s $(pwd)/schemes/dracula.zsh-theme $HOME/.oh-my-zsh/themes/dracula.zsh-theme
 
   if [[ ! -f ~/.zshrc.local ]]; then
     echo "Creating .zshrc.local"
